278d2c32c64a80a01429f7cd58644bb26d22fc95
FadyKamalIskander <66503506+FadyKamalIskander@users.noreply.github.com>
2021-07-17 18:19:54 +0200

    Update process_image.cpp

--- Project2/ball_chaser/src/process_image.cpp
+++ Project2/ball_chaser/src/process_image.cpp
@@ -34,9 +34,12 @@ void process_image_callback(const sensor_msgs::Image img)
     bool isWhitePixel = false;
     static bool isMoving = false;
     // Loop through each pixel in the image and check if it is white
-    for (int i = 0; i < img.height * img.step; i++) 
+    for (int i = 0; i < img.height * img.step; i+=3) 
     {
-        if (img.data[i] == white_pixel) 
+        if (img.data[i] == white_pixel && 
+            img.data[i + 1] == white_pixel && 
+            img.data[i + 2] == white_pixel 
+           ) 
         {
             isWhitePixel = true;
             
